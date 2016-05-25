f3f92020e6d1ef1c60acd073c394b032ab87333a
Jon Warren <jon@jonwarren.info>
2016-05-25 17:50:29 -0500

    Changed to use Math::Round->nearest() for total rounding. Allow changing precision and rounding output for totals display. Changed to use Sort::Hash->sort_has() for totals display to output descending by total then ascending by task name. Added visual separators for totals display to visually group tasks by positive, zero, and negative total times. Standardized task names with Lingua::EN::Titlecase. Extracted code from the main script into subs. General code cleanup.

--- task.pl
+++ task.pl
@@ -8,35 +8,57 @@
 #
 
 use POSIX qw(isdigit);
+use Math::Round qw(nearest);
+use Sort::Hash qw(sort_hash);
+use Lingua::EN::Titlecase;
+
+# change at your own risk -- some of the other scripts might not be forgiving
+my $tasklogs_dirname = 'tasklogs';    # main directory for all tasklogs
+my $tasklog_filename = '.hours';      # task log file, stored in $tasklogs_dirname
+my $tasklogs_backupdir = 'backups';   # subdirectory of $tasklogs_dirname
+my $quit = 'quit';                    # pseudo-taskname used to end the day
+
+# ====================================================================
+# TODO: Set these to control output.
+# #====================================================================
+# By default this will output values rounded to nearest quarter hour:
+my $rounding_precision = 0.25;
+my $seconds_conversion = 3600; # seconds to hours
+my $printf_fmt = "%5.2f";
+#
+# If you need output by tenth of an hour, use these values:
+# my $rounding_precision = 0.1;
+# my $seconds_conversion = 3600; # seconds to hours
+# my $printf_fmt = "%5.1f";
+#
+# If you need output in minutes, rounded to nearest minute:
+# my $rounding_precision = 1;
+# my $seconds_conversion = 60; # seconds to minutes
+# my $printf_fmt = "%5.0f";
+# #====================================================================
 
 #
-# Startup initialization
-#
-my $taskdir = "";
-if (defined $ENV{HOME}) {
-    $taskdir = $ENV{HOME} . "/tasklogs/";
-} elsif (defined $ENV{HOMEDRIVE} && defined $ENV{HOMEPATH}) {
-    $taskdir = $ENV{HOMEDRIVE} . $ENV{HOMEPATH} . "tasklogs\\";
-} else {
-    die "HOME or (HOMEDRIVE and HOMEPATH) environment variables not set\n";
-}
-my $logfn = $taskdir . '.hours';
-my $quit = 'quit';
-
-#
-# Scan log file to build %total, and identify most recent task.
+# Scan log file to build %totals, and identify most recent task.
 # File format is one line for each task switch:
 # time taskname
 #
+my %totals;
+my ($now, $task, $prev, $then, $diff);
+my $tc = Lingua::EN::Titlecase->new("");
+
+my $logfn = &get_logfn();
 if (-f $logfn) {
-    open INFILE, "<$logfn"
-        or die "$logfn: $!\n";
+    open INFILE, "<$logfn" or die "$logfn: $!\n";
     while (<INFILE>) {
-        ($now, $task) = split;
+        chomp;
+        ($now, $task) = split( /\s+/, $_, 2 );
+        $task = $tc->title("$task");
+
         if (defined $prev) {
             $diff = $now - $then;
-            $total{$prev} += $diff;
+            $totals{"$prev"} += $diff;
         }
+
         $prev = $task;
         $then = $now;
     }
@@ -65,21 +87,27 @@ task.pl [-<minutesago>|+(minutestoadd)] <taskname>
 eof
     exit;
 }
-elsif ($ARGV[0] =~ /^-\d+$/) {
-    # task -minutes taskname
-    my $ago = abs $ARGV[0];
+# task -minutes taskname
+elsif ($ARGV[0] =~ /^-[\d.]+$/) {
+    my $args = join( ' ', @ARGV );
+    ($mins, $task) = split( /\s+/, $args, 2 );
+    my $ago = abs $mins;
     $now = time - 60 * $ago;
-    $task = $ARGV[1];
 }
-elsif ($ARGV[0] =~ /^\+\d+$/) {
-    # task +minutes taskname
-    my $add = abs $ARGV[0];
+# task +minutes taskname
+elsif ($ARGV[0] =~ /^\+[\d.]+$/) {
+    my $args = join( ' ', @ARGV );
+    ($mins, $task) = split( /\s+/, $args, 2 );
+    my $add = abs $mins;
     $now = time + 60 * $add;
-    $task = $ARGV[1];
 }
+# task taskname, or task quit
 else {
-    # task taskname, or task quit
-    $task = $ARGV[0];
+    if (defined $ARGV[0]) {
+        $task = join( ' ', @ARGV );
+    } else {
+        $task = undef;
+    }
     $now = time;
 }
 
@@ -91,18 +119,18 @@ if (defined $prev) {
     $startprev      = &fmttimearr( \@startofprev );
 
     $diff = $now - $then;
-    $total{$prev} += $diff;
+    $totals{"$prev"} += $diff;
     $min = int (($diff + 30) / 60);
     if (!defined $task) {
-        print STDERR "Doing $prev, $min minutes, started at $startprev\n";
+        print "Doing [$prev], $min minutes, started at $startprev\n";
         exit;
     }
     elsif ($task eq $prev) {
-        print STDERR "Already doing $prev, $min minutes, started at $startprev\n";
+        print "Already doing [$prev], $min minutes, started at $startprev\n";
         exit;
     }
     else {
-        print STDERR "Was doing $prev, $min minutes\n";
+        print "Was doing [$prev], $min minutes\n";
     }
 }
 elsif (!defined $task) {
@@ -112,35 +140,244 @@ elsif (!defined $task) {
 #
 # Append new task switch record to logfile.
 #
-open OUTFILE, ">>$logfn"
-    or die "$logfn: $!\n";
+open OUTFILE, ">>$logfn" or die "$logfn: $!\n";
+$task = $tc->title("$task");
 print OUTFILE "$now $task\n";
 close OUTFILE;
 
 #
 # task quit: rotate log file and print report.
 #
-if ($task eq $quit) {
-    print STDERR "Task summary:\n";
-    while (($task, $tot) = each %total) {
-        # %f rounds to nearest
-        printf "%3.1f  %s\n", $tot / 3600, $task;
+# TODO: move this to a sub-routine
+#
+if (lc("$task") eq lc("$quit")) {
+    my $eod_filename = &get_endofday_filename();
+    open( EODFH, ">$eod_filename") or die "Could not open $eod_filename for writing";
+
+    my %rounded;
+    foreach my $tsk (keys %totals) {
+        my $r = nearest( $rounding_precision, $totals{"$tsk"} / $seconds_conversion );
+        $r = abs $r if ( $r == 0 ); # get rid of annoying -0.00 values
+        $tsk = $tc->title("$tsk");
+        $rounded{"$tsk"} = $r;
     }
-    my $daily = $taskdir . 'daily.pl';
-    print `perl $daily end`;
+
+    print "\nTask summary:";
+    print "\n==============================\n";
+
+    my $found_zero = 0;
+    foreach my $tsk (sort { $rounded{"$b"} <=> $rounded{"$a"} or lc("$a") cmp lc("$b") } keys %rounded) {
+        my $tot = $rounded{"$tsk"};
+        if ( $found_zero == 0 && $tot == 0 ) {
+            $found_zero = 1;
+            print "------------------------------\n";
+        } elsif ( $found_zero == 1 && $tot != 0 ) {
+            $found_zero = 0;
+            print "------------------------------\n";
+        }
+        printf "$printf_fmt  %s\n", $tot, $tsk;
+        printf EODFH "$printf_fmt  %s\n", $tot, $tsk;
+    }
+
+    &backup_logfn( $logfn );
+
+    # pass control to daily.pl to process end-of-day functions
+    my $daily_script = &get_daily_script();
+    exec "perl $daily_script end";
+}
+else {
+    print "Now doing [$task]\n";
+}
+
+######################################################################
+exit; # End of main script ###########################################
+######################################################################
+
+######################################################################
+# Get the daily.pl script location
+#
+# TODO: pull this out into a tasklogs suite module.
+#
+sub get_daily_script() {
+    my $taskdir = &get_taskdir();
+
+    my $daily_script = $taskdir . 'daily.pl';
+    return $daily_script;
+}
+
+######################################################################
+# Get the name of the current day
+#
+# TODO: pull this out into a tasklogs suite module.
+#
+sub get_currday_name() {
+    my @days = qw(Sunday Monday Tuesday Wednesday Thursday Friday Saturday);
+
+    my (undef,undef,undef,undef,undef,undef,$wday,undef,undef) = localtime(time);
+
+    return $days[$wday];
+}
+
+######################################################################
+# Get the name of the previous day
+#
+# TODO: pull this out into a tasklogs suite module.
+#
+sub get_prevday_name() {
+    my @days = qw(Sunday Monday Tuesday Wednesday Thursday Friday Saturday);
+
+    my (undef,undef,undef,undef,undef,undef,$wday,undef,undef) = localtime(time);
+
+    if ( $wday == 0 ) {
+        $wday = scalar(@days) - 1;
+    } else {
+        $wday--;
+    }
+
+    return $days[$wday];
+}
+
+######################################################################
+# Get the numeric YYMMDD for the current day
+#
+# TODO: pull this out into a tasklogs suite module.
+#
+sub get_currday_yymmdd() {
+    my (undef,undef,undef,$mday,$mon,$year,$wday,undef,undef) = localtime(time);
+
+    # Adjust $mon to be in the 1-12 range
+    $mon++;
+    # Adjust $mday to be Saturday
+    $mday    += (6 - $wday);
+    # Adjust $year for years >= 2000
+    $year -= 100 if ( $year >= 100 );
+    # Adjust $mon, $mday, and $year to be 2-digit numeric strings
+    $mon     =  sprintf( "%2.2d", $mon );
+    $mday    =  sprintf( "%2.2d", $mday );
+    $year    =  sprintf( "%2.2d", $year );
+
+    return "$year$mon$mday";
+}
+
+######################################################################
+# Get the numeric YYMMDD for the previous day
+#
+# TODO: pull this out into a tasklogs suite module.
+#
+sub get_prevday_yymmdd() {
+    my (undef,undef,undef,$mday,$mon,$year,$wday,undef,undef) = localtime(time);
+
+    # Adjust $mon to be in the 1-12 range
+    $mon++;
+    # Adjust $mday to be Saturday
+    $mday    += (6 - $wday);
+    # Adjust $year for years >= 2000
+    $year -= 100 if ( $year >= 100 );
+    # Adjust $mon, $mday, and $year to be 2-digit numeric strings
+    $mon     =  sprintf( "%2.2d", $mon );
+    $mday    =  sprintf( "%2.2d", $mday );
+    $year    =  sprintf( "%2.2d", $year );
+
+    return "$year$mon$mday";
+}
+
+######################################################################
+# Get the name of the end-of-day log file
+#
+# TODO: pull this out into a tasklogs suite module.
+#
+sub get_endofday_filename() {
+    my $taskdir = &get_taskdir();
+
+    my $currday = &get_currday_name();
+    my $eod = "$taskdir.$currday";
+
+    if ( ! -f "$eod" ) {
+        my $prevday = &get_prevday_name();
+        $eod = "$taskdir.$prevday";
+    }
+
+    return $eod;
+}
+
+######################################################################
+# Backup the hours log file
+#
+# TODO: pull this out into a tasklogs suite module.
+#
+sub backup_logfn($;) {
+    my $logfn = shift;
+    my $taskdir = &get_taskdir();
+
+    my $bakdir = "$taskdir" . "$tasklogs_backupdir";
+    # TODO: change this to something more robust for determining path separators
+    if (defined $ENV{HOME}) {
+        $bakdir .= "/";
+    } elsif (defined $ENV{HOMEDRIVE} && defined $ENV{HOMEPATH}) {
+        $bakdir .= "\\";
+    } else {
+        die "HOME or (HOMEDRIVE and HOMEPATH) environment variables not set\n";
+    }
+
+    if ( ! -d "$bakdir" ) {
+        mkdir "$bakdir" or die "$bakdir could not be created: $!";
+    }
+    die "$bakdir is not accessable" unless ( -x $bakdir );
+    die "$bakdir is not readable" unless ( -r $bakdir );
+    die "$bakdir is not writable" unless ( -w $bakdir );
+
+    my $bakfn = "$bakdir" . "";
+
+    rename "$logfn", "$bakfn";
+
     rename $logfn, "$logfn.bak"
         or die "$logfn.bak: $!\n";
 }
-else {
-    print STDERR "Now doing $task\n";
+
+######################################################################
+# Get the name of the tasklogs directory
+#
+# TODO: pull this out into a tasklogs suite module.
+#
+sub get_taskdir() {
+    my $taskdir = "";
+    # TODO: change this to something more robust for determining path separators
+    if (defined $ENV{HOME}) {
+        $taskdir = $ENV{HOME} . "/$tasklogs_dirname/";
+    } elsif (defined $ENV{HOMEDRIVE} && defined $ENV{HOMEPATH}) {
+        $taskdir = $ENV{HOMEDRIVE} . $ENV{HOMEPATH} . "$tasklogs_dirname\\";
+    } else {
+        die "HOME or (HOMEDRIVE and HOMEPATH) environment variables not set\n";
+    }
+
+    if ( ! -d "$taskdir" ) {
+        mkdir "$taskdir" or die "$taskdir could not be created: $!";
+    }
+    die "$taskdir is not accessable" unless ( -x $taskdir );
+    die "$taskdir is not readable" unless ( -r $taskdir );
+    die "$taskdir is not writable" unless ( -w $taskdir );
+
+    return $taskdir;
 }
 
+######################################################################
+# Get the name of the task log file
+#
+# TODO: pull this out into a tasklogs suite module.
+#
+sub get_logfn() {
+    my $taskdir = &get_taskdir();
+    my $logfn = $taskdir . $tasklog_filename;
+}
+
+######################################################################
 # Format Time Array Values as 2-digit values
 #
 # TODO: pull this out into a tasklogs suite module.
 #
 # NOTE: Copied here from daily.pl for speed of development.
 #       I know it's bad, but I don't have the time to create a pm now.
+#
 sub fmttimearr( \@ ) {
     my $timearr = shift;
     my $timestr;
@@ -184,6 +421,8 @@ sub fmttimearr( \@ ) {
     return $timestr;
 }
 
+######################################################################
+######################################################################
 __END__
 
 =head1 NAME
